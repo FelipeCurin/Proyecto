79cfe45a3feb4686916090d3d2038b959f10669b
Felipe <fe.curin@duocuc.cl>
2021-07-07 21:35:59 -0400

    Guia terminada

---
+++ app/views.py
@@ -0,0 +1,42 @@
+from app.models import Sistema, Smartphone
+from django.shortcuts import redirect, render
+
+# Create your views here.
+
+def index(request):
+    smartphones = Smartphone.objects.all()
+    sistemas = Sistema.objects.all()
+
+    contexto = {'smartphones': smartphones, 'sistemas': sistemas}
+
+    return render(request, 'index.html', contexto)
+
+def agregar(request):
+    modelo = request.POST.get('modelo','')
+    id_sistema = request.POST.get('sistema','')
+
+    sistema = Sistema.objects.get(id = id_sistema)
+    smartphone = Smartphone(modelo = modelo, sistema = sistema)
+    smartphone.save()
+
+    return redirect('index')
+
+def editar(request, id):
+    modelo = request.POST.get('modelo', '')
+    id_sistema = request.POST.get('sistema','')
+
+    sistema = Sistema.objects.get(id = id_sistema)
+    smartphone = Smartphone.objects.get(id = id)
+
+    smartphone.modelo = modelo
+    smartphone.sistema = sistema
+
+    smartphone.save()
+
+    return redirect('index')
+
+def eliminar(request, id):
+    smartphone = Smartphone.objects.get(id = id)
+    smartphone.delete()
+
+    return redirect('index')
